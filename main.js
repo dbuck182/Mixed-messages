8bce377f44418dc9a5653ed3b54b5405ae64ba07
dbuck182 <dbuck182@gmail.com>
2022-08-03 12:55:29 -0400

    creating list for messages

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log("Hello this is my script");
+
+const messages = {
+have_A : ['Great Day', 'Spectacular Day', 'Super Day'],
+you_Should: ['go workout', 'hangout with your friends', 'code a website'],
+you_Are: ['A great person', 'a good coder', 'in good shape']
+};
